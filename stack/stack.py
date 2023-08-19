146c7b6c932d3e4e62e528f43fe694e71d3b44c2
Nirvana <lamabruno55@gmail.com>
2023-08-19 14:57:21 +0545

    refactor: update naming convention of stack

---
+++ stack/stack.py
@@ -0,0 +1,35 @@
+class Stack:
+    def __init__(self) -> None:
+        self.items = []
+
+    def create_stack(self) -> list:
+        return self.items
+    
+    def check_stack_size(self) -> int:
+        size_stack = len(self.items)
+        return size_stack
+    
+    def append_value_stack(self,value) -> list:
+        self.items.append(value)
+        return self.items
+    
+    def pop_value_stack(self):
+        size_of_stack = len(self.items)
+        if size_of_stack == 0:
+            return "Stack is empty"
+        
+        self.items.pop()
+        return self.items
+    
+# Creating instance
+stack1 = Stack()
+print(stack1.create_stack())
+print(stack1.append_value_stack(2))
+print(stack1.check_stack_size())
+print(stack1.pop_value_stack(2))
+print(stack1.check_stack_size())
+
+
+    
+
+    

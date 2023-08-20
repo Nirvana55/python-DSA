1677c14a5547ee8fca9b5c5a33100118a4d69ab9
Nirvana <lamabruno55@gmail.com>
2023-08-20 18:25:14 +0545

    feat: stack backspace and compare list example

---
+++ stack/examples/backspaceCharacter.py
@@ -0,0 +1,25 @@
+#step1: loop the two string
+#step2: check if they contain # 
+#step3: if they contain # pop else append
+#step4: compare both string and check they are same or not
+
+class Solution:
+    def backspaceCompare(self,s:str,t:str) -> bool:
+        a = self.checkLoop(s)
+        b = self.checkLoop(t)
+        return a == b
+
+    def checkLoop(self,s:str) -> list:
+        stack = []
+        for i in s:
+            if i != "#":
+                stack.append(i)
+            elif len(stack):
+                stack.pop()
+        return stack
+      
+          
+s1 = Solution()
+
+print(s1.backspaceCompare("a#","a#"))
+print(s1.backspaceCompare("ab#","ac##"))

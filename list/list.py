ebe82135adb7432d3453a41843fd4468f2cb701b
Nirvana <lamabruno55@gmail.com>
2023-08-23 18:57:48 +0545

    refactor: list comprehensive example

--- list/list.py
+++ list/list.py
@@ -10,6 +10,8 @@ l1.remove(1)
 print(l1)
 # to remove the last element 
 l1.pop()
+# to find the length of the list
+len(l1)
 l1.append(3)
 l1.append(2)
 # to check how much time the element has occurred
@@ -19,6 +21,8 @@ print(l1.count(1))
 l1[:] = [1,2]
 print(l1)
 #list comprehensive
+#it is turning some loop into single line code
+#1st is the expression second is the loop and even we can add if condition
 l2 = [(x,y) for x in range(2) for y in range(3)]
 print(l2)
 

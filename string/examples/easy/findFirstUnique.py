0663bb7025869e219066eef80eb7739a483a95c9
Nirvana <lamabruno55@gmail.com>
2023-08-31 22:15:54 +0545

    feat: examples string 2

---
+++ string/examples/easy/findFirstUnique.py
@@ -0,0 +1,21 @@
+#leet code level: Easy
+
+#Question: Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+#algorithm:
+#we can loop and count 
+#or we can append
+from collections import Counter
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        new_list = Counter(s)
+        for i in range(len(s)):
+            if new_list[s[i]] == 1:
+                return i
+        return -1
+    
+s1 = Solution()
+print(s1.firstUniqChar("leetcode"))
+
+

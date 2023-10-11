ed3f0934c2dc5d1366b75798fcee473a273e001e
nirvana <lamabruno55@gmail.com>
2023-10-11 21:06:16 +0545

    chore: update breakdown of the find first and last element position

--- list/leetcode/medium/findFirstLastElementList.py
+++ list/leetcode/medium/findFirstLastElementList.py
@@ -3,24 +3,32 @@
 # Question: Need to find the first and Last position of an element
 # requirement is to find it on o(log(n))
 
+# Break down:
+# had to check the position of the element so we used the len(nums)
+# to check the left most and not start from the right
+# after finding the left most we check on the right most which is greater or equals to the target
+# by doing this we later do subtraction of -1 so that we get the actual element
+# later we check the low is less than or equal to high we return our output
+# else return the error
+
 
 class Solution:
     def searchRange(self, nums, target):
         def search(x):
-            lo, hi = 0, len(nums)
-            while lo < hi:
-                mid = (lo + hi) // 2
+            low, high = 0, len(nums)
+            while low < high:
+                mid = (low + high) // 2
                 if nums[mid] < x:
-                    lo = mid + 1
+                    low = mid + 1
                 else:
-                    hi = mid
-            return lo
+                    high = mid
+            return low
 
-        lo = search(target)
-        hi = search(target + 1) - 1
+        low = search(target)
+        high = search(target + 1) - 1
 
-        if lo <= hi:
-            return [lo, hi]
+        if low <= high:
+            return [low, high]
 
         return [-1, -1]
 
